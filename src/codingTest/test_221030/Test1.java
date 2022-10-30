4210e1407b269183008410361a931b91b598fca9
hanchaeri <coflzz@naver.com>
2022-10-30 19:00:41 +0900

    1030 프로그래머스_배열뒤집기

--- src/codingTest/test_221030/Test1.java
+++ src/codingTest/test_221030/Test1.java
@@ -3,17 +3,11 @@ package codingTest.test_221030;
 import java.util.*;
 
 public class Test1 {
-    public String solution(String my_string) {
-        String answer = "";
-        String[] string = my_string.split("");
-        String[] answerArr = new String[string.length];
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
 
-        for(int i=0; i<string.length; i++) {
-            answerArr[i] = string[string.length - 1 - i];
-        }
-
-        for(String a : answerArr) {
-            answer += a;
+        for(int i=0; i<num_list.length; i++) {
+            answer[num_list.length - i -1] = num_list[i];
         }
 
         return answer;
@@ -21,7 +15,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         Test1 test1 = new Test1();
-        String data = "abcde";
+        int[] data = {5,4,3,2,1};
         System.out.println(test1.solution(data));
     }
 }
